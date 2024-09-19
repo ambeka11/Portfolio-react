ffb1a2ee5360a16317246bc411d2a399920b2efa
Ambeka Siwakoti <ambeeka02@gmail.com>
2024-09-19 14:27:30 +0545

    added animations

---
+++ src/component/Technologies.jsx
@@ -0,0 +1,80 @@
+import { RiReactjsLine } from "react-icons/ri";
+import { TbBrandNextjs } from "react-icons/tb";
+import { SiMongodb } from "react-icons/si";
+import { DiRedis } from "react-icons/di";
+import { FaNodeJs } from "react-icons/fa";
+import { BiLogoPostgresql } from "react-icons/bi";
+import {motion} from 'framer-motion'
+
+const iconVariants = (duration) => ({
+  initial: {y: -10},
+  animate: {
+    y: [10, -10], 
+    transition: {
+      duration: duration, 
+      ease: 'linear',
+      repeat: Infinity,
+      repeatType: 'reverse',
+    },
+  }
+})
+
+const Technologies = () => {
+  return (
+    <div className="border-b border-neutral-800 pb-24">
+      <h1 className="my-20 text-center text-4xl">Technologies</h1>
+      <div className="flex flex-wrap items-center justify-center gap-5">
+        <motion.div
+        variants={iconVariants(2.5)}
+        initial="initial"
+        animate="animate" 
+         className="rounded-2xl border-4 border-neutral-800 p-5">
+          <RiReactjsLine className="text-7xl text-cyan-400" />
+        </motion.div>
+
+        <motion.div 
+        variants={iconVariants(4.1)}
+        initial="initial"
+        animate="animate"      
+        className="rounded-2xl border-4 border-neutral-800 p-5">
+          <TbBrandNextjs className="text-7xl" />
+        </motion.div>
+
+        <motion.div 
+                variants={iconVariants(5)}
+                initial="initial"
+                animate="animate"
+        className="rounded-2xl border-4 border-neutral-800 p-5">
+          <SiMongodb className="text-7xl text-green-500" />
+        </motion.div>
+
+        <motion.div 
+                variants={iconVariants(2)}
+                initial="initial"
+                animate="animate"
+        className="rounded-2xl border-4 border-neutral-800 p-5">
+          <DiRedis className="text-7xl text-red-700" />
+        </motion.div>
+
+        <motion.div 
+                variants={iconVariants(6)}
+                initial="initial"
+                animate="animate"
+        className="rounded-2xl border-4 border-neutral-800 p-5">
+          <FaNodeJs className="text-7xl text-green-500" />
+        </motion.div>
+
+        <motion.div 
+                variants={iconVariants(4)}
+                initial="initial"
+                animate="animate"
+        className="rounded-2xl border-4 border-neutral-800 p-5">
+          <BiLogoPostgresql className="text-7xl text-sky-700" />
+        </motion.div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Technologies;
